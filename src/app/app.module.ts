78a01bd04d2689b9eb5708101c09acc36fefe345
yangguanxiong <yang.grayson@gmail.com>
2017-02-22 21:25:20 -0500

    use angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { DataService } from './services/data.service'
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DataService } from './services/data.service'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [
     DataService
