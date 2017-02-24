3679b75af598c1d8c92aef248fdc4bf4225ab552
yangguanxiong <yang.grayson@gmail.com>
2017-02-24 12:05:25 -0500

    minor change

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,13 +5,10 @@ import { Observable } from 'rxjs/Rx';
 @Injectable()
 export class DataService {
 
-  private key: string = 'YXV0aGVudGljYTpAdXRoM250MWNA'; // encoded authentica:@uth3nt1c@
-
   constructor(private _http: Http) { }
 
   getStudents(): Observable<IStudent[]> {
     let authHeader: Headers = new Headers();
-    // authHeader.append('Ocp-Apim-Subscription-Key', this.key); 
     authHeader.append('Authorization', 'Basic ' + btoa('authentica:@uth3nt1c@'));
 
     let url: string = 'http://interviewapi20170221095727.azurewebsites.net/api/Student/All';
